cb6c5a1eb59ee4e626fe9b7d9418e196a18ed28c
Andy18j <115467259+Andy18j@users.noreply.github.com>
2024-03-31 18:53:01 +0530

    a

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -43,7 +43,7 @@ userRouter.post('/login', async (req, res) => {
 
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.status(200).json({msg: "Login successful" });
+        res.status(200).json({msg: "Login successful" ,token});
     } catch (err) {
         console.error(err);
         res.status(500).json({ msg: "Internal server error" });
